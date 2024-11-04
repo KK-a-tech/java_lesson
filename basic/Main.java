43bf40f65999bfacdfbcdc33d9996bc403f621ad
KK-a-tech <kento.k711@gmail.com>
2024-11-04 14:36:53 +0900

    feat:大きい数値の表示(e3)

--- basic/Main.java
+++ basic/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println(-8);
         System.out.println(2.5);
         System.out.println(1_000_000);
+        System.out.println(1.2e3);
+        System.out.println(1.2e-3);
     }
 }
