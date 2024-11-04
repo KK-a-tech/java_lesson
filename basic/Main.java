ba8e0f5a959ab35e7e78c3b83650bb19d0cb20bb
KK-a-tech <kento.k711@gmail.com>
2024-11-04 14:41:23 +0900

    feat:コメントの記法

--- basic/Main.java
+++ basic/Main.java
@@ -2,11 +2,17 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        /*
+          Javaにおける数値表現
+          author: Kento
+        */
         System.out.println(10);
         System.out.println(-8);
         System.out.println(2.5);
         System.out.println(1_000_000);
-        System.out.println(1.2e3);
-        System.out.println(1.2e-3);
+        // System.out.println(1_000_000); `ctrl+/`
+        // eを使った表現
+        System.out.println(1.2e3); // 1.2かける10の3乗
+        System.out.println(1.2e-3); // 1.2かける10の-3乗
     }
 }
