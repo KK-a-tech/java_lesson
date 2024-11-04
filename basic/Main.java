45a6f2170277c859aa3a7e8f374f4833685a62a4
KK-a-tech <kento.k711@gmail.com>
2024-11-04 14:34:17 +0900

    add:数字の表示

---
+++ basic/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(10);
+        System.out.println(-8);
+        System.out.println(2.5);
+        System.out.println(1_000_000);
+    }
+}
